2811fa90bbadf804a75d81193bd43ab9b8aeef8d
winnie1201 <92582911+winnie1201@users.noreply.github.com>
2022-01-12 20:46:01 +0800

    Add files via upload

--- week-3/week3.py
+++ week-3/week3.py
@@ -6,8 +6,8 @@ with request.urlopen(src) as response:
 clist=data["result"]["results"]
 s="景點名稱,區域,經度,緯度,第一張圖檔網址\n"
 for clists in clist:
-        print(clists["stitle"],clists["address"][5:8],clists["longitude"],clists["latitude"])
-        print(clists["file"].lower().replace('jpg','jpg\n').split("\n")[0])
+        clists["stitle"],clists["address"][5:8],clists["longitude"],clists["latitude"]
+        clists["file"].lower().replace('jpg','jpg\n').split("\n")[0]
         s+="{},{},{},{},{}\n".format(clists["stitle"],clists["address"][5:8],clists["longitude"],clists["latitude"],clists["file"].lower().replace('jpg','jpg\n').split("\n")[0])
 fw=open("week3.csv","w",encoding="utf-8")
 fw.write(s)
